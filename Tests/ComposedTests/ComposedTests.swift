97b1fd23f979ad57f2ffed3c9b3b6e945226d8ab
Anders Mannberg <anders.mannberg@softhouse.se>
2020-05-06 21:03:28 +0200

    Test refactor

--- Tests/ComposedTests/ComposedTests.swift
+++ Tests/ComposedTests/ComposedTests.swift
@@ -29,41 +29,34 @@ final class ComposedTests: XCTestCase {
     
     func test_can_access_properties_from_composed_object() {
         let monkey = MoneyMakingBoozeMonkey(
-            Monkey(name: "Chico", hasTail: true),
-            MoneyMaker(salary: 100000),
-            Boozehound(poisonOfChoice: "Bourbon")
+            Monkey(name: "Rutger", hasTail: true),
+            MoneyMaker(salary: 10),
+            Boozehound(poisonOfChoice: "Anything")
         )
         
-        XCTAssertTrue(monkey.name == "Chico")
+        XCTAssertTrue(monkey.name == "Rutger")
         XCTAssertTrue(monkey.hasTail)
-        XCTAssertTrue(monkey.salary == 100000)
-        XCTAssertTrue(monkey.poisonOfChoice == "Bourbon")
+        XCTAssertTrue(monkey.salary == 10)
+        XCTAssertTrue(monkey.poisonOfChoice == "Anything")
     }
     
     func test_can_compose_composed_object_and_access_all_properties() {
         typealias MoneyMakingBoozeMonkeyFilmBuff = Compose2<MoneyMakingBoozeMonkey, FilmBuff>
         
         let monkey = MoneyMakingBoozeMonkeyFilmBuff(
-            MoneyMakingBoozeMonkey(
-                Monkey(name: "Chico", hasTail: true),
-                MoneyMaker(salary: 100000),
-                Boozehound(poisonOfChoice: "Bourbon")
-            ),
+            MoneyMakingBoozeMonkey.standard,
             FilmBuff(favoriteMovie: "Jaws")
         )
         
-        XCTAssertTrue(monkey.name == "Chico")
-        XCTAssertTrue(monkey.hasTail)
-        XCTAssertTrue(monkey.salary == 100000)
-        XCTAssertTrue(monkey.poisonOfChoice == "Bourbon")
+        XCTAssertTrue(monkey.name == "Ralph")
+        XCTAssertFalse(monkey.hasTail)
+        XCTAssertTrue(monkey.salary == 0)
+        XCTAssertTrue(monkey.poisonOfChoice == "Brandy")
         XCTAssertTrue(monkey.favoriteMovie == "Jaws")
     }
     
     func test_can_encode_composed_object_to_json_string() {
-        let monkey = MoneyMakingMonkey(
-            Monkey(name: "Chico", hasTail: true),
-            MoneyMaker(salary: 100000)
-        )
+        let monkey: MoneyMakingBoozeMonkey = .standard
         let data = try! JSONEncoder().encode(monkey)
         let string = String(data: data, encoding: .utf8)
         XCTAssertNotNil(string)
@@ -86,6 +79,14 @@ final class ComposedTests: XCTestCase {
 
 fileprivate typealias MoneyMakingMonkey = Compose2<Monkey, MoneyMaker>
 fileprivate typealias MoneyMakingBoozeMonkey = Compose3<Monkey, MoneyMaker, Boozehound>
+
+fileprivate extension MoneyMakingBoozeMonkey {
+    static let standard: MoneyMakingBoozeMonkey = MoneyMakingBoozeMonkey(
+        Monkey(name: "Ralph", hasTail: false),
+        MoneyMaker(salary: 0),
+        Boozehound(poisonOfChoice: "Brandy")
+    )
+}
 
 fileprivate struct Monkey: Codable {
     let name: String
