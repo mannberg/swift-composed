0a71091d8857caa0ee883b4fbc79c282ea7cf419
Anders Mannberg <anders.mannberg@softhouse.se>
2020-05-06 20:26:26 +0200

    Can compose 2 and 3 objects

--- Sources/Composed/Composed.swift
+++ Sources/Composed/Composed.swift
@@ -1,3 +1,43 @@
-struct Composed {
-    var text = "Hello, World!"
+@dynamicMemberLookup
+struct Compose2<A,B> {
+    let a: A
+    let b: B
+    
+    init(_ a: A, _ b: B) {
+        self.a = a
+        self.b = b
+    }
+    
+    subscript<X>(dynamicMember keyPath: KeyPath<A, X>) -> X {
+        return a[keyPath: keyPath]
+    }
+    
+    subscript<X>(dynamicMember keyPath: KeyPath<B, X>) -> X {
+        return b[keyPath: keyPath]
+    }
+}
+
+@dynamicMemberLookup
+struct Compose3<A,B,C> {
+    let a: A
+    let b: B
+    let c: C
+    
+    init(_ a: A, _ b: B, _ c: C) {
+        self.a = a
+        self.b = b
+        self.c = c
+    }
+    
+    subscript<X>(dynamicMember keyPath: KeyPath<A, X>) -> X {
+        return a[keyPath: keyPath]
+    }
+    
+    subscript<X>(dynamicMember keyPath: KeyPath<B, X>) -> X {
+        return b[keyPath: keyPath]
+    }
+    
+    subscript<X>(dynamicMember keyPath: KeyPath<C, X>) -> X {
+        return c[keyPath: keyPath]
+    }
 }
--- Tests/ComposedTests/ComposedTests.swift
+++ Tests/ComposedTests/ComposedTests.swift
@@ -6,10 +6,78 @@ final class ComposedTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Composed().text, "Hello, World!")
     }
 
     static var allTests = [
         ("testExample", testExample),
     ]
+    
+    func test_can_create_a_composed_object_from_two_structs() {
+        _ = MoneyMakingMonkey(
+            Monkey(name: "Chico", hasTail: true),
+            MoneyMaker(salary: 100000)
+        )
+    }
+    
+    func test_can_create_a_composed_object_from_three_structs() {
+        _ = MoneyMakingBoozeMonkey(
+            Monkey(name: "Chico", hasTail: true),
+            MoneyMaker(salary: 100000),
+            Boozehound(poisonOfChoice: "Bourbon")
+        )
+    }
+    
+    func test_can_access_properties_from_composed_object() {
+        let monkey = MoneyMakingBoozeMonkey(
+            Monkey(name: "Chico", hasTail: true),
+            MoneyMaker(salary: 100000),
+            Boozehound(poisonOfChoice: "Bourbon")
+        )
+        
+        XCTAssertTrue(monkey.name == "Chico")
+        XCTAssertTrue(monkey.hasTail)
+        XCTAssertTrue(monkey.salary == 100000)
+        XCTAssertTrue(monkey.poisonOfChoice == "Bourbon")
+    }
+    
+    func test_can_compose_composed_object_and_access_all_properties() {
+        typealias MoneyMakingBoozeMonkeyFilmBuff = Compose2<MoneyMakingBoozeMonkey, FilmBuff>
+        
+        let monkey = MoneyMakingBoozeMonkeyFilmBuff(
+            MoneyMakingBoozeMonkey(
+                Monkey(name: "Chico", hasTail: true),
+                MoneyMaker(salary: 100000),
+                Boozehound(poisonOfChoice: "Bourbon")
+            ),
+            FilmBuff(favoriteMovie: "Jaws")
+        )
+        
+        XCTAssertTrue(monkey.name == "Chico")
+        XCTAssertTrue(monkey.hasTail)
+        XCTAssertTrue(monkey.salary == 100000)
+        XCTAssertTrue(monkey.poisonOfChoice == "Bourbon")
+        XCTAssertTrue(monkey.favoriteMovie == "Jaws")
+    }
+}
+
+//MARK: Helpers
+
+fileprivate typealias MoneyMakingMonkey = Compose2<Monkey, MoneyMaker>
+fileprivate typealias MoneyMakingBoozeMonkey = Compose3<Monkey, MoneyMaker, Boozehound>
+
+fileprivate struct Monkey {
+    let name: String
+    let hasTail: Bool
+}
+
+fileprivate struct MoneyMaker {
+    let salary: Int
+}
+
+fileprivate struct Boozehound {
+    let poisonOfChoice: String
+}
+
+fileprivate struct FilmBuff {
+    let favoriteMovie: String
 }
